f09e19976ccc79ae014ca1630ba39c4c49f3a979
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-03-22 04:30:26 +0530

    CRUD operations on user using CLI

---
+++ src/main/java/com/bootocoding/User/command/impl/ExitCommand.java
@@ -0,0 +1,11 @@
+package com.bootocoding.User.command.impl;
+
+import com.bootocoding.User.command.Command;
+import com.bootocoding.User.model.Result;
+
+public class ExitCommand implements Command {
+    @Override
+    public Result execute(String[] attributes) throws Exception {
+        return null;
+    }
+}
